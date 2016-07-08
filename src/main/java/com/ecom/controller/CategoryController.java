dd3c46865a0fae4b74eb26684498b52914df6a94
Naris Razafimahatratra <nrazafimahatratra@valueit.fr>
2016-07-08 15:54:01 +0300

    implement category controller

---
+++ src/main/java/com/ecom/controller/CategoryController.java
@@ -0,0 +1,7 @@
+package com.ecom.controller;
+
+/**
+ * Created by Workdev on 08/07/2016.
+ */
+public class CategoryController {
+}
---
+++ src/main/java/com/ecom/document/Vendor.java
@@ -0,0 +1,7 @@
+package com.ecom.document;
+
+/**
+ * Created by Workdev on 08/07/2016.
+ */
+public class Vendor {
+}
